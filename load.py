b0f6cfde5d9e0fe21ded100a036ee91ccca9f0e8
svjt <josephtoppo.sxd@gmail.com>
2025-06-12 00:21:49 +0530

    Added load.py

--- load.py
+++ load.py
@@ -1,6 +1,26 @@
-def load_assets(addr:str):
-     with open(addr, 'r') as file:
-          print(file)
+import os
 
-a = load_assets('./assets')
-print(a)
+def load_asset(addr:str):
+     txt_files_data = {}
+
+     if not os.path.isdir(addr):
+         raise ValueError(f"Provided path '{addr}' is not a directory.")
+     
+     for filename in os.listdir(addr):
+          file_path = os.path.join(addr, filename)
+
+          if not os.path.isfile(file_path):
+               continue
+
+          try:
+               with open(file_path, "r", encoding="utf-8") as file:
+                    content = file.read()
+                    txt_files_data[filename] = content
+          except (UnicodeDecodeError, OSError) as e:
+               print(f"Error reading file {filename}: {e}")
+     
+     return txt_files_data
+
+a = load_asset("assets/colorscripts/regular/")
+for i in a:
+     print(i)
